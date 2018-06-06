dee5b56ce17921d6e705ba28f5d7257b0794a852
sriram1998 <sriramvenkat98@gmail.com>
2018-06-06 15:51:20 +0530

    rectify row filtering

--- extract.py
+++ extract.py
@@ -17,49 +17,29 @@ from nltk.tokenize import word_tokenize
 
 from tqdm import tqdm
 stopwords=set(stopwords.words('english'))
-data=pd.read_csv('csv/github_issues_sampledv4.csv',usecols = ['issue_title','body'] , encoding='utf8')
+data=pd.read_csv('csv/github_issues_sampledv2.csv',usecols = ['issue_title','body'] , encoding='utf8')
 df = pd.DataFrame(data,columns=['issue_title','body'])
 rem=0
 
+#replace contractions like can't to cannot.
 def replace_cont(text):
  return contractions.fix(text)
 
-
-def check_common(t,b):
- result = False
- for x in t:
-  for y in b:
-   if x == y:
-    result = True
-    return result 
- return result
-
 k=0
 
+#checks for filtered rows.
 def check_atl2(t , b):
- #if len(list(set(t).intersection(b)))>=1:
  check=0
- #print "original:"
- #print list(set(t).intersection(b))
- if len(list(set(t).intersection(b)))>1:
+ if len(list(set(t).intersection(b)))>2:
   check=1
-  #print "filtered"
-  #print list(set(t).intersection(b))
-  #print "check"
-  #print check
   return check 
- #print "check fail"
- #print check
  return check
- #else:
- 	#return False
-  
-
-
+ 
 for i , row in df.iterrows():
  title=data['issue_title'][i]
  body=data['body'][i]
- print i
+ if type(body)==float:
+  continue
  title_tokens=word_tokenize(title)
  body_tokens=word_tokenize(body)
  title_fil=[w for w in title_tokens if not w in stopwords]
@@ -78,15 +58,10 @@ for i , row in df.iterrows():
  		w=replace_cont(w)
  		body_fil.append(w)
  
- if check_common(title_fil , body_fil) == False:
-  rem=rem+1
+ 
  if check_atl2(title_fil , body_fil)==1:
   k=k+1  
  		
-
-
-
-
 print "total:"
 print k
 #print rem
---
+++ git_issues_sampling.py
@@ -0,0 +1,24 @@
+import collections
+import pandas as pd
+import numpy as np
+import nltk
+
+dfi = pd.DataFrame(columns=['issue_title','body'])
+i=0
+#this iterates through chinks of 10000.
+for df in pd.read_csv('csv/github_issues.csv' , usecols = ['issue_title','body'] , iterator=True , chunksize=10000):
+ #set the value of i to select chunk.
+ if i>50:
+  dfi=pd.concat([dfi,df] , sort=False)
+ i=i+1
+ if i>60:
+  break;
+ 
+ 
+print dfi.shape
+dfi.to_csv('csv/github_issues_sampledv4.csv' , encoding='utf-8' , index=False)
+
+
+
+
+
