53b82d04a6aabab3b753924b310c5fdc7451aed5
sriram1998 <sriramvenkat98@gmail.com>
2018-06-14 13:07:16 +0530

    update analyze.py

--- analyze.py
+++ analyze.py
@@ -8,36 +8,61 @@ from nltk.stem import LancasterStemmer, WordNetLemmatizer
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
-vectorizer = CountVectorizer()
-vectorizer2 = TfidfVectorizer()
+from collections import defaultdict
+import csv
+from sklearn.feature_extraction.text import TfidfVectorizer
+tf1 = TfidfVectorizer(analyzer='word', ngram_range=(0,1), min_df = 0, stop_words = 'english')
+tf2 = TfidfVectorizer(analyzer='word', ngram_range=(0,1), min_df = 0)
+tf3 = TfidfVectorizer(analyzer='word', ngram_range=(2,2), min_df = 0, stop_words = 'english')
+tf4 = TfidfVectorizer(analyzer='word', ngram_range=(2,2), min_df = 0)
+tf5 = TfidfVectorizer(analyzer='word', ngram_range=(3,3), min_df = 0, stop_words = 'english')
+tf6 = TfidfVectorizer(analyzer='word', ngram_range=(3,3), min_df = 0)
 
-#reading the csv file and creating dataframe.
-data=pd.read_csv('csv/github_issues_preproct1.csv',usecols=['issue_title' , 'body'],encoding='utf8')
-df = pd.DataFrame(data,columns=['issue_title','body'])
-titlev=[]
-bodyv=[]
+desc=defaultdict(list)
 
-for i , row in df.iterrows():
- title=data['issue_title'][i]
- body=data['body'][i]
- if type(body)==float:
-  continue
- title_tokens=word_tokenize(title)
- body_tokens=word_tokenize(body)
+with open('csv/preproct1f.csv',  'r') as sentences_file:
+	reader=csv.reader(sentences_file , delimiter=",")
+	reader.next()
+	for row in reader:
+		desc[row[1]].append(row[2])
+
+for desc_id , text in desc.iteritems():
+	desc[desc_id]="".join(text)
+
+corpus=[]
+
+for id , desc in sorted(desc.iteritems()):
+	corpus.append(desc)
  
- for w in title_tokens:
-  titlev.append(w)
- for w in body_tokens:
-  bodyv.append(w) 
- vectorizer.fit(titlev)
- vector = vectorizer.transform(titlev)
- vectorizer2.fit(bodyv)
- vector2=vectorizer2.transform(bodyv)
+def analysis(row , n , s):
+ if n==1 and s==1:
+  tfidf_matrix=tf1.fit_transform(corpus)
+  feature_names=tf1.get_feature_names()
+ if n==1 and s==0:
+  tfidf_matrix=tf2.fit_transform(corpus)
+  feature_names=tf2.get_feature_names()	
+ if n==2 and s==1:
+  tfidf_matrix=tf3.fit_transform(corpus)
+  feature_names=tf3.get_feature_names()
+ if n==2 and s==0:
+  tfidf_matrix=tf4.fit_transform(corpus)
+  feature_names=tf4.get_feature_names()
+ if n==3 and s==1:
+  tfidf_matrix=tf5.fit_transform(corpus)
+  feature_names=tf5.get_feature_names()
+ if n==3 and s==0:
+  tfidf_matrix=tf6.fit_transform(corpus)
+  feature_names=tf6.get_feature_names()
+ 
+ dense=tfidf_matrix.todense()
 
-#print vectorizer2.vocabulary_
-print "count vectoriZer"
-print vector.shape
-print vector2.shape
-print "TfiDf"
-print vector
-print(vector2.todense())
+ text1=dense[row].tolist()[0]
+ phrase_scores = [pair for pair in zip(range(0, len(text1)), text1) if pair[1] > 0]
+
+ a= sorted(phrase_scores, key=lambda t: t[1] * -1)[:5]	
+ for i in range(0,5):
+  print feature_names[a[i][0]]
+ 
+
+
+analysis(51 , 3 , 0)
